cc2fb3c7c6978ba2ed83cd5677a45cdcb56bcb8c
bfren <ben@bcgdesign.com>
2021-09-24 08:00:07 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,17 @@
 
 set -euo pipefail
 
+# Path is required
 [[ -z "${1-}" ]] && \
   echo "::error::You must pass the file path." && \
   exit 1
 
+# Read contents
 CONTENTS=$(cat ${1})
 
+# Escape new lines
+CONTENTS="${CONTENTS//$'\n'/'%0A'}"
+CONTENTS="${CONTENTS//$'\r'/'%0D'}"
+
+# Output contents
 echo "::set-output name=contents::${CONTENTS}"
